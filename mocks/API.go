a6f388b382ebdfa7d5704fb801e52544a1ecc1f3
Nathan Smith <nate@nathan@neocortical.net>
2016-08-01 20:27:13 -0700

    update mocks

--- mocks/API.go
+++ mocks/API.go
@@ -9,68 +9,155 @@ type API struct {
 	mock.Mock
 }
 
-func (m *API) Entry(channel string, entrytype string, params url.Values) (goib.Item, error) {
-	ret := m.Called(channel, entrytype, params)
+// Entry provides a mock function with given fields: channel, entrytype, params
+func (_m *API) Entry(channel string, entrytype string, params url.Values) (goib.Item, error) {
+	ret := _m.Called(channel, entrytype, params)
 
-	r0 := ret.Get(0).(goib.Item)
-	r1 := ret.Error(1)
+	var r0 goib.Item
+	if rf, ok := ret.Get(0).(func(string, string, url.Values) goib.Item); ok {
+		r0 = rf(channel, entrytype, params)
+	} else {
+		r0 = ret.Get(0).(goib.Item)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, string, url.Values) error); ok {
+		r1 = rf(channel, entrytype, params)
+	} else {
+		r1 = ret.Error(1)
+	}
 
 	return r0, r1
 }
-func (m *API) Search(channel string, query string, params url.Values) (*goib.Collection, error) {
-	ret := m.Called(channel, query, params)
+
+// Search provides a mock function with given fields: channel, query, params
+func (_m *API) Search(channel string, query string, params url.Values) (*goib.Collection, error) {
+	ret := _m.Called(channel, query, params)
 
 	var r0 *goib.Collection
-	if ret.Get(0) != nil {
-		r0 = ret.Get(0).(*goib.Collection)
+	if rf, ok := ret.Get(0).(func(string, string, url.Values) *goib.Collection); ok {
+		r0 = rf(channel, query, params)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*goib.Collection)
+		}
 	}
-	r1 := ret.Error(1)
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, string, url.Values) error); ok {
+		r1 = rf(channel, query, params)
+	} else {
+		r1 = ret.Error(1)
+	}
 
 	return r0, r1
 }
-func (m *API) Content(channel string, contentID int, params url.Values) (goib.Item, error) {
-	ret := m.Called(channel, contentID, params)
 
-	r0 := ret.Get(0).(goib.Item)
-	r1 := ret.Error(1)
+// Content provides a mock function with given fields: channel, contentID, params
+func (_m *API) Content(channel string, contentID int, params url.Values) (goib.Item, error) {
+	ret := _m.Called(channel, contentID, params)
+
+	var r0 goib.Item
+	if rf, ok := ret.Get(0).(func(string, int, url.Values) goib.Item); ok {
+		r0 = rf(channel, contentID, params)
+	} else {
+		r0 = ret.Get(0).(goib.Item)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, int, url.Values) error); ok {
+		r1 = rf(channel, contentID, params)
+	} else {
+		r1 = ret.Error(1)
+	}
 
 	return r0, r1
 }
-func (m *API) ContentMedia(channel string, contentID int, params url.Values) ([]goib.Item, error) {
-	ret := m.Called(channel, contentID, params)
+
+// ContentMedia provides a mock function with given fields: channel, contentID, params
+func (_m *API) ContentMedia(channel string, contentID int, params url.Values) ([]goib.Item, error) {
+	ret := _m.Called(channel, contentID, params)
 
 	var r0 []goib.Item
-	if ret.Get(0) != nil {
-		r0 = ret.Get(0).([]goib.Item)
+	if rf, ok := ret.Get(0).(func(string, int, url.Values) []goib.Item); ok {
+		r0 = rf(channel, contentID, params)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]goib.Item)
+		}
 	}
-	r1 := ret.Error(1)
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, int, url.Values) error); ok {
+		r1 = rf(channel, contentID, params)
+	} else {
+		r1 = ret.Error(1)
+	}
 
 	return r0, r1
 }
-func (m *API) ContentItems(channel string, contentID int, params url.Values) ([]goib.Item, error) {
-	ret := m.Called(channel, contentID, params)
+
+// ContentItems provides a mock function with given fields: channel, contentID, params
+func (_m *API) ContentItems(channel string, contentID int, params url.Values) ([]goib.Item, error) {
+	ret := _m.Called(channel, contentID, params)
 
 	var r0 []goib.Item
-	if ret.Get(0) != nil {
-		r0 = ret.Get(0).([]goib.Item)
+	if rf, ok := ret.Get(0).(func(string, int, url.Values) []goib.Item); ok {
+		r0 = rf(channel, contentID, params)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]goib.Item)
+		}
 	}
-	r1 := ret.Error(1)
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, int, url.Values) error); ok {
+		r1 = rf(channel, contentID, params)
+	} else {
+		r1 = ret.Error(1)
+	}
 
 	return r0, r1
 }
-func (m *API) Closings(channel string, filter goib.ClosingsFilter, providerID ...string) (goib.ClosingsResponse, error) {
-	ret := m.Called(channel, filter, providerID)
 
-	r0 := ret.Get(0).(goib.ClosingsResponse)
-	r1 := ret.Error(1)
+// Closings provides a mock function with given fields: channel, filter, providerID
+func (_m *API) Closings(channel string, filter goib.ClosingsFilter, providerID ...string) (goib.ClosingsResponse, error) {
+	ret := _m.Called(channel, filter, providerID)
+
+	var r0 goib.ClosingsResponse
+	if rf, ok := ret.Get(0).(func(string, goib.ClosingsFilter, ...string) goib.ClosingsResponse); ok {
+		r0 = rf(channel, filter, providerID...)
+	} else {
+		r0 = ret.Get(0).(goib.ClosingsResponse)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, goib.ClosingsFilter, ...string) error); ok {
+		r1 = rf(channel, filter, providerID...)
+	} else {
+		r1 = ret.Error(1)
+	}
 
 	return r0, r1
 }
-func (m *API) UnmarshalReceiver(r goib.Receiver) (goib.Item, error) {
-	ret := m.Called(r)
 
-	r0 := ret.Get(0).(goib.Item)
-	r1 := ret.Error(1)
+// UnmarshalReceiver provides a mock function with given fields: r
+func (_m *API) UnmarshalReceiver(r goib.Receiver) (goib.Item, error) {
+	ret := _m.Called(r)
+
+	var r0 goib.Item
+	if rf, ok := ret.Get(0).(func(goib.Receiver) goib.Item); ok {
+		r0 = rf(r)
+	} else {
+		r0 = ret.Get(0).(goib.Item)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(goib.Receiver) error); ok {
+		r1 = rf(r)
+	} else {
+		r1 = ret.Error(1)
+	}
 
 	return r0, r1
 }
--- mocks/Item.go
+++ mocks/Item.go
@@ -7,38 +7,72 @@ type Item struct {
 	mock.Mock
 }
 
-func (m *Item) GetType() goib.ItemType {
-	ret := m.Called()
+// GetType provides a mock function with given fields:
+func (_m *Item) GetType() goib.ItemType {
+	ret := _m.Called()
 
-	r0 := ret.Get(0).(goib.ItemType)
-
-	return r0
-}
-func (m *Item) GetContentID() int {
-	ret := m.Called()
-
-	r0 := ret.Get(0).(int)
-
-	return r0
-}
-func (m *Item) GetTeaserTitle() string {
-	ret := m.Called()
-
-	r0 := ret.Get(0).(string)
+	var r0 goib.ItemType
+	if rf, ok := ret.Get(0).(func() goib.ItemType); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(goib.ItemType)
+	}
 
 	return r0
 }
-func (m *Item) GetTeaserText() string {
-	ret := m.Called()
 
-	r0 := ret.Get(0).(string)
+// GetContentID provides a mock function with given fields:
+func (_m *Item) GetContentID() int {
+	ret := _m.Called()
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func() int); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int)
+	}
 
 	return r0
 }
-func (m *Item) GetPublicationDate() int64 {
-	ret := m.Called()
 
-	r0 := ret.Get(0).(int64)
+// GetTeaserTitle provides a mock function with given fields:
+func (_m *Item) GetTeaserTitle() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	return r0
+}
+
+// GetTeaserText provides a mock function with given fields:
+func (_m *Item) GetTeaserText() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	return r0
+}
+
+// GetPublicationDate provides a mock function with given fields:
+func (_m *Item) GetPublicationDate() int64 {
+	ret := _m.Called()
+
+	var r0 int64
+	if rf, ok := ret.Get(0).(func() int64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
 
 	return r0
 }
